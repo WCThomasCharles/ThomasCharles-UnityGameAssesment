eee30c8ecd8304f652e56d74678d04583598b137
WCThomasCharles <19232@wc.school.nz>
2022-04-11 11:10:03 +1200

    Ver.7
    Worked on levels, added tutorial

--- Unity Assesment game/Assets/Scripts/TitleManager.cs
+++ Unity Assesment game/Assets/Scripts/TitleManager.cs
@@ -5,29 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour
 {
-    // The current canvas to display
-    // - 0 is the start screen
-    // - 1 is the level selector
-    private int canavsToDisplay = 0;
-
     public GameObject startScreen;
     public GameObject levelSelect;
 
-    private void Update()
-    {
-        if (SceneManager.GetActiveScene().name == "TitleScreen")
-        {
-            if (canavsToDisplay == 0) startScreen.SetActive(true);
-            else if (canavsToDisplay != 0) startScreen.SetActive(false);
+    public GameObject tutorialScreen1;
+    public GameObject tutorialScreen2;
+    public GameObject tutorialScreen3;
 
-            if (canavsToDisplay == 1) levelSelect.SetActive(true);
-            else if (canavsToDisplay != 1) levelSelect.SetActive(false);
-        }
-    }
-
-    public void SetCanvas(int canvasNumber)
+    public void SwitchCanvas(int canvasToDisplay)
     {
-        canavsToDisplay = canvasNumber;
+        // - 0 is the start screen
+        // - 1 is the level selector
+        // - 2, 3, and 4 are tutorial screens 1, 2, and 3
+
+        if (canvasToDisplay == 0) startScreen.SetActive(true);
+        else if (canvasToDisplay != 0) startScreen.SetActive(false);
+
+        if (canvasToDisplay == 1) levelSelect.SetActive(true);
+        else if (canvasToDisplay != 1) levelSelect.SetActive(false);
+
+        if (canvasToDisplay == 2) tutorialScreen1.SetActive(true);
+        else if (canvasToDisplay != 2) tutorialScreen1.SetActive(false);
+
+        if (canvasToDisplay == 3) tutorialScreen2.SetActive(true);
+        else if (canvasToDisplay != 3) tutorialScreen2.SetActive(false);
+
+        if (canvasToDisplay == 4) tutorialScreen3.SetActive(true);
+        else if (canvasToDisplay != 4) tutorialScreen3.SetActive(false);
     }
 
     public void GoToScene(string sceneToActivate)
