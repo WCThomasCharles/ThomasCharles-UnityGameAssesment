9cd997132d8530a7e7a804ed8680cd4f608ae862
WCThomasCharles <19232@wc.school.nz>
2022-04-14 10:11:20 +1200

    Ver.9
    Worked on an in level HUD, refined levels

--- Unity Assesment game/Assets/Scripts/GameManager.cs
+++ Unity Assesment game/Assets/Scripts/GameManager.cs
@@ -8,6 +8,14 @@ public class GameManager : MonoBehaviour
     public GameObject victoryScreen; // The screen to show when the player wins
     public GameObject gameOverScreen; // The screen to show when the player loses
 
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("r"))
+        {
+            RestartThisLevel();
+        }
+    }
 
     // GoToScene is a function that can be called by buttons to switch scenes
     // it takes the name of the scene the button switches to as an argument
